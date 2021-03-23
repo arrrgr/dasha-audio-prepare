49ee3b832850762dc91bba9da9819a855535a24e
Andrey Chernykh <andrey.chernykh@dasha.ai>
2021-03-23 12:22:52 +0700

    fix after review

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-Sample application based on FFMPEG (https://ffmpeg.org/download.html)
+Sample application based on FFMPEG
 
 Can be used to normalize and strip silence in the audio
 
-just run `node index.js srcDir dstDir`
+just run 
+* `npm i`
+* `node index.js srcDir dstDir`
 
 Programm will process each `wav` and `mp3` file and put processed `mp3` to `dstDir` with the same name
 
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const exec = require("child_process");
-const tmp = require("tmp");
 const fs = require("fs")
 const path = require("path");
-const { ENOENT } = require("constants");
+const pathToFfmpeg = require('ffmpeg-static');
 
 class FfmpegException extends Error {
     constructor(message) {
@@ -10,57 +9,68 @@ class FfmpegException extends Error {
     }
 }
 
-async function runFFMPEG(args) {
-    const ffmpeg = await exec.spawnSync(
-        "ffmpeg", args, { encoding: "utf8" }
+function runFFMPEG(args) {
+    const ffmpeg = exec.spawnSync(
+        pathToFfmpeg, args, { encoding: "utf8", stdio: "inherit" }
     );
     if (ffmpeg.error !== undefined) {
-        console.log(ffmpeg.error.code);
-        if (ffmpeg.error.code === "ENOENT") {
-            throw new FfmpegException(`ffmpeg not found, install it first`);
-        }
         throw new FfmpegException(`ffmpeg exited: ${ffmpeg.error}`);
     }
-    if (ffmpeg.status != 0) {
-        throw new FfmpegException(`ffmpeg exited: ${ffmpeg.status} ${ffmpeg.stderr}`);
+    if (ffmpeg.status !== 0) {
+        throw new FfmpegException(`ffmpeg exited: ${ffmpeg.status}`);
     }
 }
 
-async function Process(src, dst) {
+function processAudio(src, dst) {
     const leftPad = 100;
     const rightPad = 400;
-    const filters = `loudnorm=i=-15,silenceremove=1:0.1:-50dB,areverse,silenceremove=1:0.1:-50dB,adelay=${rightPad},areverse,adelay=${leftPad}`;
-    await runFFMPEG(["-i", src, "-ac", "1", "-af", filters, dst, "-y"]);
+    const filters = [
+        "afftdn", //remove noice
+        "loudnorm=i=-15", //normalize volume
+        "silenceremove=1:0.1:-50dB", //remove left silence
+        "areverse", //reverse audio
+        "silenceremove=1:0.1:-50dB", //remove right audio
+        `adelay=${rightPad}`, //add padding to the right
+        "areverse", //reverse back
+        `adelay=${leftPad}` //add padding to left
+    ].join(",");
+    const args = [
+        "-i", //input file
+        src,
+        "-ac",
+        "1", //single channel
+        "-af", //set filter graph
+        filters,
+        dst, //target file
+        "-y" //force rewrite
+    ];
+    runFFMPEG(args);
 }
 
 
-async function main() {
+function main() {
     const srcDir = process.argv[2];
     const dstDir = process.argv[3];
-    let hasErrors = false;
-    if (srcDir == undefined || dstDir == undefined) {
+    if (srcDir === undefined || dstDir === undefined) {
         console.log("node index.js <src_dir> <dst_dir>");
         return;
     }
-    var entries = await fs.promises.readdir(srcDir);
-    await fs.promises.mkdir(dstDir, { recursive: true });
-    for (const i in entries) {
-        var ext = path.extname(entries[i]).toLowerCase();
-        if (ext == ".wav" || ext == ".mp3") {
-            const src = path.join(srcDir, entries[i]);
-            const dst = path.join(dstDir, path.parse(entries[i]).name + ".mp3");
+    const entries = fs.readdirSync(srcDir);
+    fs.mkdirSync(dstDir, { recursive: true });
+    for (const entry of entries) {
+        const ext = path.extname(entry).toLowerCase();
+        if (ext === ".wav" || ext === ".mp3") {
+            const src = path.join(srcDir, entry);
+            const dst = path.join(dstDir, path.parse(entry).name + ".mp3");
             console.log(`processing ${src} to ${dst}`);
             try {
-                await Process(src, dst);
+                processAudio(src, dst);
             } catch (e) {
                 console.error(`Failed to convert ${src}`, e);
-                hasErrors = true;
+                process.exitCode = 1;
             }
         }
     }
-    if (hasErrors)
-        process.exit(1);
-    process.exit(0);
 }
 
 
